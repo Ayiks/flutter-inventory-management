faaadb37dfae327d72a0195250f295b49635c64c
Ayiks <cliffysarp@gmail.com>
2023-05-25 09:15:51 +0000

    model update

--- lib/app/data/models/order/order.freezed.dart
+++ lib/app/data/models/order/order.freezed.dart
@@ -1,7 +1,7 @@
 // coverage:ignore-file
 // GENERATED CODE - DO NOT MODIFY BY HAND
 // ignore_for_file: type=lint
-// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target
+// ignore_for_file: unused_element, deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies, annotate_overrides, invalid_annotation_target, unnecessary_question_mark
 
 part of 'order.dart';
 
@@ -166,6 +166,7 @@ class _$_Order implements _Order {
   final Map<String, BasketItem> _orderDetails;
   @override
   Map<String, BasketItem> get orderDetails {
+    if (_orderDetails is EqualUnmodifiableMapView) return _orderDetails;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableMapView(_orderDetails);
   }
@@ -390,6 +391,7 @@ class _$_OrderDTO implements _OrderDTO {
   final Map<String, BasketItem> _orderDetails;
   @override
   Map<String, BasketItem> get orderDetails {
+    if (_orderDetails is EqualUnmodifiableMapView) return _orderDetails;
     // ignore: implicit_dynamic_type
     return EqualUnmodifiableMapView(_orderDetails);
   }
