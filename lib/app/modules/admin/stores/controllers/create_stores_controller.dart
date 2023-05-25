886b485cfbed204fe9f66f33515843cf93e0f94b
Ayiks <cliffysarp@gmail.com>
2023-05-25 15:14:48 +0000

    controller for creating stores

---
+++ lib/app/modules/admin/stores/controllers/create_stores_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+
+class CreateStoresController extends GetxController {
+  //TODO: Implement CreateStoresController
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {
+    super.onClose();
+  }
+}
