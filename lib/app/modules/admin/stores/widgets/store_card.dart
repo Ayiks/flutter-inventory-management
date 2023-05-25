5c5b2e0e7e0a9028adcf6befdf1d15fa844e4765
Ayiks <cliffysarp@gmail.com>
2023-05-25 15:15:53 +0000

    custom widget for store stats

---
+++ lib/app/modules/admin/stores/widgets/store_card.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:inventory_1/app/modules/admin/stores/widgets/store_info.dart';
+import 'package:inventory_1/app/utils/dimmension.dart';
+
+class StoreCard extends StatelessWidget {
+  const StoreCard({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(Dimensions.height15),
+      // height: 200,
+      // color: Colors.black,
+      decoration: BoxDecoration(
+          border: Border.all(),
+          borderRadius: const BorderRadius.all(Radius.circular(30))),
+      child: const Column(
+        children: [
+          const ListTile(
+            title: Text('Cold Store'),
+            trailing: Icon(Icons.more_horiz),
+          ),
+          const Divider(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              StoreInfo(
+                title: 'Products',
+                subtitle: '200',
+              ),
+              StoreInfo(title: 'Total Cost', subtitle: 'Ghc 5,000'),
+              StoreInfo(title: 'Total Cost', subtitle: 'Ghc 5,000')
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
