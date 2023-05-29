82c2302078221b9848b79727a14a6dd0b760a915
Ayiks <cliffysarp@gmail.com>
2023-05-29 15:29:25 +0000

    removal of dart:async

--- lib/app/modules/admin/stores/controllers/stores_controller.dart
+++ lib/app/modules/admin/stores/controllers/stores_controller.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get/get.dart';
 import 'package:inventory_1/app/data/models/store/store.dart';
+import 'package:inventory_1/app/modules/admin/stores/controllers/edit_store_controller.dart';
+import 'package:inventory_1/app/modules/admin/stores/widgets/store_action_model.dart';
 
 class StoresController extends GetxController {
   // final RxList<Store> _allStores = RxList([]);
@@ -10,6 +10,9 @@ class StoresController extends GetxController {
   // set allStores(List<Store> value) => _allStores.value = value;
 
   RxList<Store> storeList = RxList<Store>([]);
+
+  final EditStoreController editStoreController =
+      Get.find<EditStoreController>();
 
   @override
   void onInit() {
@@ -40,5 +43,10 @@ class StoresController extends GetxController {
   @override
   void onClose() {
     super.onClose();
+  }
+
+  void showAlertDialog({required Store store}) {
+    editStoreController.store = store;
+    Get.dialog(StoreActionModal(store: store));
   }
 }
