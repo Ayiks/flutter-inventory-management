99568b29a7ee5d6d6aec703021987a3b379837f8
Ayiks <cliffysarp@gmail.com>
2023-05-29 15:27:22 +0000

    updated stored binding with editing controller

--- lib/app/modules/admin/stores/bindings/stores_binding.dart
+++ lib/app/modules/admin/stores/bindings/stores_binding.dart
@@ -1,10 +1,13 @@
 import 'package:get/get.dart';
+import 'package:inventory_1/app/modules/admin/stores/controllers/edit_store_controller.dart';
 
 import '../controllers/stores_controller.dart';
 
 class StoresBinding extends Bindings {
   @override
   void dependencies() {
+    Get.lazyPut<EditStoreController>(() => EditStoreController(), fenix: true);
+
     Get.lazyPut<StoresController>(
       () => StoresController(),
     );
