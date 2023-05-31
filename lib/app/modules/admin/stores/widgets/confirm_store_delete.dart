67c1fb383d7f82c50370d31294a95e9641180e07
Ayiks <cliffysarp@gmail.com>
2023-05-31 15:18:45 +0000

    custom dialog for confirming delete

---
+++ lib/app/modules/admin/stores/widgets/confirm_store_delete.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:inventory_1/app/data/models/store/store.dart';
+import 'package:inventory_1/app/modules/admin/stores/controllers/stores_controller.dart';
+import 'package:inventory_1/app/utils/dimmension.dart';
+
+class ConfirmStoreDeleteAlertDialog extends GetView<StoresController> {
+  final Store store;
+
+  const ConfirmStoreDeleteAlertDialog({
+    required this.store,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Icon(
+        Icons.warning_amber_rounded,
+        color: Colors.red,
+        size: Dimensions.font26 * 2,
+      ),
+      content: const Text(
+          'This action will delete every data related this store.\n\n NB: This action is NOT reversible, do you want to proceed?'),
+      actions: [
+        TextButton(
+            onPressed: () {
+              Get.back();
+              Get.back();
+            },
+            child: Text(
+              'No',
+              style: TextStyle(fontSize: Dimensions.font20),
+            )),
+        TextButton(
+            onPressed: () {
+              // TODO: complete confirmation.
+              // controller.onDeleteProductConfirmed(product);
+            },
+            child: Text(
+              'Yes',
+              style: TextStyle(color: Colors.red, fontSize: Dimensions.font16),
+            ))
+      ],
+    );
+  }
+}
