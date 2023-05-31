19cb3e599a331578a4d139eda8b620797fc07c29
Ayiks <cliffysarp@gmail.com>
2023-05-31 15:13:38 +0000

    store editing

--- lib/app/modules/admin/stores/views/edit_store_view.dart
+++ lib/app/modules/admin/stores/views/edit_store_view.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
-import 'package:inventory_1/app/data/models/store/store.dart';
 import 'package:inventory_1/app/utils/dimmension.dart';
 
 import '../controllers/edit_store_controller.dart';
@@ -20,17 +19,69 @@ class EditStoreView extends GetView<EditStoreController> {
         padding: EdgeInsets.all(Dimensions.height15),
         children: [
           Obx(() {
-            Store? store = controller.store;
+            // Store? store = controller.store;
             return Form(
+                key: controller.formKey,
                 child: Column(
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                      prefixIcon: Icon(Icons.store_outlined),
-                      border: OutlineInputBorder()),
-                )
-              ],
-            ));
+                  children: [
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(Icons.store_outlined),
+                          prefixIconColor: Colors.red,
+                          border: OutlineInputBorder()),
+                      initialValue: controller.store?.name,
+                      validator: controller.validateName,
+                      onChanged: controller.setStoreName,
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    TextFormField(
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.location_on_outlined,
+                            color: Colors.green,
+                          ),
+                          border: OutlineInputBorder()),
+                      initialValue: controller.store?.location,
+                      validator: controller.validateLocation,
+                      onChanged: controller.setStoreLocation,
+                    ),
+                    SizedBox(height: Dimensions.height15),
+                    TextFormField(
+                      maxLines: 3,
+                      keyboardType: TextInputType.streetAddress,
+                      decoration: const InputDecoration(
+                          prefixIcon: Icon(
+                            Icons.location_city_outlined,
+                            color: Colors.blue,
+                          ),
+                          border: OutlineInputBorder()),
+                      initialValue: controller.store?.address,
+                      onChanged: controller.setStoreAddress,
+                    ),
+                    SizedBox(height: Dimensions.height45),
+                    GestureDetector(
+                      onTap: controller.updateStore,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: const Color.fromARGB(255, 16, 81, 133),
+                            borderRadius:
+                                BorderRadius.circular(Dimensions.radius15 - 5)),
+                        height: Dimensions.height10 * 5,
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                            child: Text(
+                          'Save Changes',
+                          style: TextStyle(
+                              fontSize: Dimensions.font16,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        )),
+                      ),
+                    ),
+                  ],
+                ));
           })
         ],
       )),
