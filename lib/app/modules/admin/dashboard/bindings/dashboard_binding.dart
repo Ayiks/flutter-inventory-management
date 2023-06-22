366fe8f05651a0e06ada33d22595473744d446a8
Ayiks <cliffysarp@gmail.com>
2023-06-22 19:28:19 +0000

    WIP

--- lib/app/modules/admin/dashboard/bindings/dashboard_binding.dart
+++ lib/app/modules/admin/dashboard/bindings/dashboard_binding.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:inventory_1/app/modules/admin/stores/controllers/stores_controller.dart';
+import 'package:inventory_1/app/modules/sales_person/first_page/controllers/first_page_controller.dart';
 
 import '../controllers/dashboard_controller.dart';
 
@@ -10,9 +11,7 @@ class DashboardBinding extends Bindings {
       () => DashboardController(),
     );
 
-    Get.lazyPut<StoresController>(
-      () => StoresController(),
-      fenix: true,
-    );
+    Get.lazyPut<StoresController>(() => StoresController());
+    // Get.lazyPut<FirstPageController>(() => FirstPageController());
   }
 }
