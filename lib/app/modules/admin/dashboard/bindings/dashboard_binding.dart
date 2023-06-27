2490d1db35b6754dae8485ac110973679f46a580
Ayiks <cliffysarp@gmail.com>
2023-06-27 17:54:26 +0000

    removal of unused packages

--- lib/app/modules/admin/dashboard/bindings/dashboard_binding.dart
+++ lib/app/modules/admin/dashboard/bindings/dashboard_binding.dart
@@ -1,6 +1,6 @@
 import 'package:get/get.dart';
 import 'package:inventory_1/app/modules/admin/stores/controllers/stores_controller.dart';
-import 'package:inventory_1/app/modules/sales_person/first_page/controllers/first_page_controller.dart';
+// import 'package:inventory_1/app/modules/sales_person/first_page/controllers/first_page_controller.dart';
 
 import '../controllers/dashboard_controller.dart';
 
