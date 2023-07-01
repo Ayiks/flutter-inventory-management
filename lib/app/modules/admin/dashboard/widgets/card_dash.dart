19b898f072b2ac04824072cdced7fcbd05619a85
Ayiks <cliffysarp@gmail.com>
2023-07-01 05:29:14 +0000

    redesign

--- lib/app/modules/admin/dashboard/widgets/card_dash.dart
+++ lib/app/modules/admin/dashboard/widgets/card_dash.dart
@@ -5,7 +5,7 @@ class StatCard extends StatelessWidget {
   final IconData icon;
   final String label;
   final Color? color;
-  final num value;
+  final String value;
   final void Function()? onTap;
 
   const StatCard({
@@ -21,41 +21,32 @@ class StatCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        height: Dimensions.height20 * 5,
-        width: Dimensions.width20 * 9,
-        decoration: BoxDecoration(
-            color: Colors.black12, borderRadius: BorderRadius.circular(10)),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Icon(
-                  icon,
-                  size: Dimensions.iconSize24 + 6,
-                  color: color,
-                ),
-                Text(
-                  '$value',
-                  style: TextStyle(
-                      fontSize: Dimensions.font20 + 5,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(
-              height: Dimensions.height20,
-            ),
-            Text(
-              label,
-              style: TextStyle(
-                  fontSize: Dimensions.font16, fontWeight: FontWeight.w600),
-            )
-          ],
-        ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            children: [
+              Icon(
+                icon,
+                size: Dimensions.iconSize24 + 6,
+                color: color,
+              ),
+              Text(
+                label,
+                style: TextStyle(
+                    fontSize: Dimensions.font16, fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: Dimensions.height10,
+          ),
+          Text(
+            value,
+            style: TextStyle(
+                fontSize: Dimensions.font20 + 5, fontWeight: FontWeight.bold),
+          )
+        ],
       ),
     );
   }
